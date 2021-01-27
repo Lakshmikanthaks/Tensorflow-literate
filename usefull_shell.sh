ee3500e43fd72e09e4ae11510e546c1d48623034
Lakshmikantha K S <lakshmikanth.shirahatti@gmail.com>
2021-01-27 12:46:57 +0530

    Update usefull_shell.sh

--- usefull_shell.sh
+++ usefull_shell.sh
@@ -3,3 +3,6 @@ ffmpeg -ss xx.xx -i input.wav -t yy.yy -c copy output.wav
 
 #Concatinate 2 wave files into one wave file 
 sox input1.wav input2.wav output.wav
+
+#Split 4 channel audio input to seperate channel using ffmpeg 
+ffmpeg -i test.wav -map_channel 0.0.0 0.wav -map_channel 0.0.1 1.wav -map_channel 0.0.2 2.wav -map_channel 0.0.3 3.wav
