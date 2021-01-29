e50277a9864e9e1ff41a5d9749be72668d2a0918
Lakshmikantha K S <lakshmikanth.shirahatti@gmail.com>
2021-01-30 02:01:46 +0530

    Update usefull_shell.sh

--- usefull_shell.sh
+++ usefull_shell.sh
@@ -6,3 +6,6 @@ sox input1.wav input2.wav output.wav
 
 #Split 4 channel audio input to seperate channel using ffmpeg 
 ffmpeg -i test.wav -map_channel 0.0.0 0.wav -map_channel 0.0.1 1.wav -map_channel 0.0.2 2.wav -map_channel 0.0.3 3.wav
+
+#recording multi channel (4 Channel) with 16000 Sample rate audio in Raspberry Pi for Respeaker 4 Mic Hardware 
+sudo arecord -Dac108 -f S32_LE -r 16000 -c 4 103_all.wav
